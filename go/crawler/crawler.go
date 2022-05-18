8a7ba679fe9470ed9870b19ff4a9b199a1ad6d5d
Zizico2 <berna.agua@gmail.com>
2022-05-18 01:38:22 +0100

    folder reorganization

--- go/crawler/crawler.go
+++ go/crawler/crawler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"unicode"
 
@@ -14,37 +15,33 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type parsingStage uint
+
+const (
+	parsingStageundefined parsingStage = iota
+	parsingStagecharacteristics
+	parsingStagepreRequisits
+	parsingStageexams
+)
+
+type examsStage uint
+
+const (
+	examsStageUndefined examsStage = iota
+)
+
 type examsType uint
 
 const (
-	mandatory       examsType = iota
-	groups          examsType = iota
-	undiferentiated examsType = iota
+	examsTypeUndefined examsType = iota
+	examsTypeMandatory
+	examsTypeGroups
 )
 
-type examsParsed struct {
-	description string
-	exams       []exam
-	examGroups  [][]exam
-}
-
-type examParsed struct {
-	code uint
-	name string
-}
-
-type courseInfoParsed struct {
-	courseName      string
-	institutionName string
-	courseCode      string // should be uint
-	institutionCode string // should be uint
-	degree          string
-	ects            uint
-	cnaef           string
-	duration        string // parse this
-	ctype           string
-	contest         string
-	examsInfo       examsInfo
+type newExamsInfo struct {
+	mandatoryExams     []exam
+	optionalExams      []exam
+	optionalExamGroups [][]exam
 }
 
 type examsInfo struct {
@@ -55,21 +52,31 @@ type examsInfo struct {
 }
 
 type exam struct {
-	code string
+	code uint8
 	name string
 }
 
+type cnaef struct {
+	code uint8
+	name string
+}
+
+type duration struct {
+	code uint8
+	name string // semestres / anos
+}
+
 type courseInfo struct {
-	courseName      string
-	institutionName string
-	courseCode      string
-	institutionCode string
-	degree          string
-	ects            string
-	cnaef           string
-	duration        string
-	ctype           string
-	contest         string
+	courseName      string //! not needed
+	institutionName string //! not needed
+	courseCode      string // foreign key
+	institutionCode string // foreign key
+	degree          string // foreign key
+	ects            uint16
+	cnaef           string // split into code + name
+	duration        string // duration struct
+	ctype           string // foreign key
+	contest         string // foreign key
 	examsInfo       examsInfo
 }
 
@@ -96,7 +103,7 @@ func main() {
 		course := courseInfo{}
 		course.courseName = e.ChildText("div.cab1")
 		course.institutionName = e.ChildText("div.cab2")
-		course.examsInfo.etype = mandatory
+		course.examsInfo.etype = examsTypeMandatory
 		examsStage := -1
 
 		groupExams := false
@@ -107,24 +114,27 @@ func main() {
 			if examsStage > 0 {
 				if !s.Is("br") {
 					if text != "" {
-						if !strings.Contains(text, "ou") && !strings.Contains(text, "Observações") && !strings.Contains(text, "e") {
+						if !strings.Contains(text, "ou") && !strings.Contains(text, "Observações") && !strings.Contains(text, "  e") {
 							arr := strings.SplitN(text, " ", 2)
 
 							if groupExams {
 								if len(course.examsInfo.examGroups) <= group {
+									code, _ := strconv.ParseUint(strings.TrimSpace(arr[0]), 10, 8) //TODO: Error handling
 									course.examsInfo.examGroups = append(course.examsInfo.examGroups, []exam{{
-										strings.TrimSpace(arr[0]),
+										uint8(code),
 										strings.TrimSpace(arr[1]),
 									}})
 								} else {
+									code, _ := strconv.ParseUint(strings.TrimSpace(arr[0]), 10, 8) //TODO: Error handling
 									course.examsInfo.examGroups[group] = append(course.examsInfo.examGroups[group], exam{
-										strings.TrimSpace(arr[0]),
+										uint8(code),
 										strings.TrimSpace(arr[1]),
 									})
 								}
 							} else {
+								code, _ := strconv.ParseUint(strings.TrimSpace(arr[0]), 10, 8) //TODO: Error handling
 								course.examsInfo.exams = append(course.examsInfo.exams, exam{
-									strings.TrimSpace(arr[0]),
+									uint8(code),
 									strings.TrimSpace(arr[1]),
 								})
 							}
@@ -140,16 +150,17 @@ func main() {
 				if strings.Contains(text, ":") {
 					course.examsInfo.description = strings.TrimSpace(text)
 					if strings.Contains(text, "conjuntos") {
-						course.examsInfo.etype = groups
+						course.examsInfo.etype = examsTypeGroups
 						groupExams = true
 					}
 				} else {
-					course.examsInfo.etype = undiferentiated
+					course.examsInfo.etype = examsTypeUndefined
 					course.examsInfo.description = "N/D"
 					if text != "" {
 						arr := strings.SplitN(text, " ", 2)
+						code, _ := strconv.ParseUint(strings.TrimSpace(arr[0]), 10, 8) //TODO: Error handling
 						course.examsInfo.exams = append(course.examsInfo.exams, exam{
-							strings.TrimSpace(arr[0]),
+							uint8(code),
 							strings.TrimSpace(arr[1]),
 						})
 					}
@@ -164,7 +175,8 @@ func main() {
 			} else if strings.Contains(text, "ECTS: ") {
 				sub := strings.Split(text, "ECTS: ")
 				sube := strings.TrimSpace(sub[1])
-				course.ects = sube
+				ects, _ := strconv.ParseUint(sube, 10, 8) //TODO: Error handling
+				course.ects = uint16(ects)
 			} else if strings.Contains(text, "Área CNAEF: ") {
 				sub := strings.Split(text, "Área CNAEF: ")
 				sube := strings.TrimSpace(sub[1])
@@ -196,7 +208,7 @@ func main() {
 		log.Println("Visiting", r.URL)
 	})
 
-	for r := 'z'; r <= 'z'; r++ {
+	for r := 'a'; r <= 'z'; r++ {
 		R := unicode.ToUpper(r)
 		c.Visit("https://www.dges.gov.pt/guias/indcurso.asp?letra=" + string(R))
 	}
@@ -223,24 +235,91 @@ func main() {
 			f.WriteString("Typo de Ensino: " + element.ctype + "\n")
 			f.WriteString("Grau: " + element.degree + "\n")
 			f.WriteString("Duração: " + element.duration + "\n")
-			f.WriteString("ECTS: " + element.ects + "\n")
+			f.WriteString("ECTS: " + fmt.Sprint(element.ects) + "\n")
 			f.WriteString("Exams description: " + element.examsInfo.description + "\n")
 			for _, exam := range element.examsInfo.exams {
-				println("hy")
-				f.WriteString("Exam code: " + exam.code + "\n")
+				f.WriteString("Exam code: " + fmt.Sprint(exam.code) + "\n")
 				f.WriteString("Exam name: " + exam.name + "\n")
 			}
 			for i, exams := range element.examsInfo.examGroups {
-				println("hy")
 				f.WriteString(fmt.Sprint(i) + "\n")
 				for _, exam := range exams {
-					f.WriteString("Exam code: " + exam.code + "\n")
+					f.WriteString("Exam code: " + fmt.Sprint(exam.code) + "\n")
 					f.WriteString("Exam name: " + exam.name + "\n")
 				}
 			}
 			f.WriteString("-------------------" + "\n")
 			f.Sync()
 		}
+	}
+
+}
+
+/*
+Escolhe uma das seguintes provas
+Escolhe um dos seguintes conjuntos
+e
+ou
+
+...
+**/
+
+func parseExams(text string, s *goquery.Selection, examsStage int, groupExams bool, group int, course *courseInfo) {
+	if examsStage > 0 {
+		if !s.Is("br") {
+			if text != "" {
+				if !strings.Contains(text, "ou") && !strings.Contains(text, "Observações") && !strings.Contains(text, "  e") {
+					arr := strings.SplitN(text, " ", 2)
+
+					if groupExams {
+						if len(course.examsInfo.examGroups) <= group {
+							code, _ := strconv.ParseUint(strings.TrimSpace(arr[0]), 10, 8) //TODO: Error handling
+							course.examsInfo.examGroups = append(course.examsInfo.examGroups, []exam{{
+								uint8(code),
+								strings.TrimSpace(arr[1]),
+							}})
+						} else {
+							code, _ := strconv.ParseUint(strings.TrimSpace(arr[0]), 10, 8) //TODO: Error handling
+							course.examsInfo.examGroups[group] = append(course.examsInfo.examGroups[group], exam{
+								uint8(code),
+								strings.TrimSpace(arr[1]),
+							})
+						}
+					} else {
+						code, _ := strconv.ParseUint(strings.TrimSpace(arr[0]), 10, 8) //TODO: Error handling
+						course.examsInfo.exams = append(course.examsInfo.exams, exam{
+							uint8(code),
+							strings.TrimSpace(arr[1]),
+						})
+					}
+				} else {
+					group++
+				}
+			}
+			if s.Is("a") {
+				examsStage = -1
+			}
+		}
+	} else if examsStage == 0 {
+		if strings.Contains(text, ":") {
+			course.examsInfo.description = strings.TrimSpace(text)
+			if strings.Contains(text, "conjuntos") {
+				course.examsInfo.etype = examsTypeGroups
+				groupExams = true
+			}
+		} else {
+			course.examsInfo.etype = examsTypeUndefined
+			course.examsInfo.description = "N/D"
+			if text != "" {
+				arr := strings.SplitN(text, " ", 2)
+				code, _ := strconv.ParseUint(strings.TrimSpace(arr[0]), 10, 8) //TODO: Error handling
+				course.examsInfo.exams = append(course.examsInfo.exams, exam{
+					uint8(code),
+					strings.TrimSpace(arr[1]),
+				})
+			}
+		}
+		examsStage = 1
 	}
 
 }
