7ea8af5a24b291f1710ee837ea3050b2774d7057
Zizico2 <berna.agua@gmail.com>
2022-06-17 00:05:24 +0100

    v0.0.2 wip

--- migrations/create_tables/up.sql
+++ migrations/create_tables/up.sql
@@ -1,73 +1,59 @@
 CREATE TABLE exams (
     rowid INTEGER NOT NULL,
-    code TEXT NOT NULL,
-    name TEXT NOT NULL,
-    PRIMARY KEY(rowid),
-    UNIQUE(rowid),
-    UNIQUE(code),
-    UNIQUE(name)
+    code TEXT UNIQUE,
+    name TEXT UNIQUE,
+    PRIMARY KEY(rowid)
 );
 
 CREATE TABLE cnaef_areas (
-    rowid INTEGER NOT NULL,
-    code TEXT NOT NULL,
-    name TEXT NOT NULL,
-    PRIMARY KEY(rowid),
-    UNIQUE(rowid),
-    UNIQUE(code),
-    UNIQUE(name)
+    rowid INTEGER NOT NULL UNIQUE,
+    code TEXT UNIQUE,
+    name TEXT UNIQUE,
+    PRIMARY KEY(rowid)
 );
 
 CREATE TABLE duration_units (
-    rowid INTEGER NOT NULL,
-    name TEXT NOT NULL,
-    PRIMARY KEY(rowid),
-    UNIQUE(rowid),
-    UNIQUE(name)
+    rowid INTEGER NOT NULL UNIQUE,
+    name TEXT UNIQUE,
+    PRIMARY KEY(rowid)
 );
 
 CREATE TABLE durations (
-    rowid INTEGER NOT NULL,
-    unit INTEGER NOT NULL,
-    ammount INTEGER NOT NULL,
-    UNIQUE(rowid),
+    rowid INTEGER NOT NULL UNIQUE,
+    unit INTEGER UNIQUE,
+    ammount INTEGER UNIQUE,
     PRIMARY KEY(rowid),
-    FOREIGN KEY(rowid) REFERENCES main(rowid) DEFERRABLE INITIALLY DEFERRED,
-    FOREIGN KEY(unit) REFERENCES duration_units(rowid) DEFERRABLE INITIALLY DEFERRED
+    FOREIGN KEY(rowid) REFERENCES main(rowid),
+    FOREIGN KEY(unit) REFERENCES duration_units(rowid)
 );
 
 CREATE TABLE institutions (
-    rowid INTEGER NOT NULL,
-    code TEXT NOT NULL,
-    name TEXT NOT NULL,
+    rowid INTEGER NOT NULL UNIQUE,
+    code TEXT UNIQUE,
+    name TEXT UNIQUE,
     /* should be an array of lines - abstract as table */
-    address TEXT NOT NULL,
+    address TEXT UNIQUE,
     /* should be an array of numbers - abstract as table */
-    phone_numbers TEXT NOT NULL,
+    phone_numbers TEXT UNIQUE,
     /* should be an array of email addresses - abstract as table */
-    email_addresses TEXT NOT NULL,
-    UNIQUE(rowid),
-    PRIMARY KEY(rowid),
-    UNIQUE(code),
-    FOREIGN KEY(rowid) REFERENCES main(rowid) DEFERRABLE INITIALLY DEFERRED
+    email_addresses TEXT UNIQUE,
+    PRIMARY KEY(rowid)
 );
 
 CREATE TABLE mandatory_exams (
-    rowid INTEGER NOT NULL,
-    exam INTEGER NOT NULL,
-    main INTEGER NOT NULL,
-    PRIMARY KEY(rowid),
-    UNIQUE(rowid),
-    FOREIGN KEY(exam) REFERENCES exams(rowid) DEFERRABLE INITIALLY DEFERRED,
-    FOREIGN KEY(main) REFERENCES main(rowid) DEFERRABLE INITIALLY DEFERRED
+    rowid INTEGER NOT NULL UNIQUE,
+    exam INTEGER UNIQUE,
+    main INTEGER UNIQUE,
+    PRIMARY KEY(rowid)
+    FOREIGN KEY(exam) REFERENCES exams(rowid)/* DEFERRABLE INITIALLY DEFERRED*/,
+    FOREIGN KEY(main) REFERENCES main(rowid)
 );
 
 CREATE TABLE main (
-    rowid INTEGER NOT NULL,
-    ects INTEGER NOT NULL,
-    institution INTEGER NOT NULL,
-    UNIQUE(rowid),
+    rowid INTEGER NOT NULL UNIQUE,
+    ects INTEGER,
+    institution INTEGER,
     PRIMARY KEY(rowid),
-    FOREIGN KEY(institution) REFERENCES institutions(rowid) DEFERRABLE INITIALLY DEFERRED,
-    FOREIGN KEY(rowid) REFERENCES durations(rowid) DEFERRABLE INITIALLY DEFERRED
+    FOREIGN KEY(institution) REFERENCES institutions(rowid),
+    FOREIGN KEY(rowid) REFERENCES durations(rowid)
 );
